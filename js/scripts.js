59f8228afc83f1fdd288b647a964b1a449a8d5d4
Sean Goldsmith <goldsmith.sean90@gmail.com>
2020-06-04 21:42:58 -0700

    Added logic to populate recipe list and get shopping list from api.

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+async function getRecipeList() {
+    let recipeList = document.getElementById('recipeList');
+    let recipes = axios('http:localhost:3200/recipes/').then(data => {
+        data.data.forEach(element => {
+            let newElem = document.createElement('li');
+            let newText = document.createTextNode(element.name);
+            newElem.setAttribute('recipeData',JSON.stringify(element));
+            newElem.appendChild(newText);
+            newElem.onclick=() => {
+                addToUsingList(newElem);
+            }
+            recipeList.appendChild(newElem);
+        });
+    })
+}
+
+function addToUsingList(recipeElem) {
+    let clone = recipeElem.cloneNode(true);
+    let usingList = document.getElementById('recipiesUsedList');
+    usingList.appendChild(clone);
+}
+
+function printShoppingList(data) {
+    let listOfKeys = Object.keys(data);
+    let text = "";
+    listOfKeys.forEach(elem => {
+        text += `${elem}: ${data[elem].amount} ${data[elem].measure} <br>`;
+    })
+    document.getElementById('shoppingListOutput').innerHTML=text;
+}
+
+async function generateRecipeList() {
+    let dataToSend = [];
+    let recipiesUsedList = document.getElementById('recipiesUsedList');
+    let listOfChildren = recipiesUsedList.getElementsByTagName('li');
+    for(i=0;i<listOfChildren.length;i++) {
+        dataToSend.push(JSON.parse(listOfChildren[i].getAttribute('recipeData')));
+    }
+    console.log(dataToSend)
+    return dataToSend;
+}
+
+async function getShoppingList() {
+    generateRecipeList().then(data => {
+        axios.post('http:/localhost:3200/new-shopping-list/',
+            data
+        ).then(result => printShoppingList(result.data)).catch(err => {
+            console.log(err.response);
+        })
+    })
+}
+
+
+
+getRecipeList();
+
+
